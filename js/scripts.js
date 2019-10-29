c4dd853ae8fe37ad122f4838cf63e1f8188d656f
Student <example@example.com>
2019-10-29 11:30:36 -0700

    add HTML and javascript/Jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,36 @@
 $(document).ready(function(){
+$("#inputform").submit(function(event){
+  event.preventDefault();
 
+  var sentence = $("input#UserInput").val();
+
+  var sentenceArray = sentence.split("");
+
+for (var i = 0; i<=sentenceArray.length; i+=1) {
+  if (sentenceArray[i]==="a") {
+    $("p").append("-");
+    console.log("test includes a")
+
+  }
+  else if (sentenceArray[i]==="e"){
+    $("p").append("-");
+  }
+
+  else if (sentenceArray[i]==="i"){
+    $("p").append("-");
+  }
+
+  else if (sentenceArray[i]==="o"){
+    $("p").append("-");
+  }
+  else if (sentenceArray[i]==="u"){
+    $("p").append("-");
+  }
+  else  {
+    $("p").append(sentenceArray[i])
+  }
+
+
+  }
+});
 });
